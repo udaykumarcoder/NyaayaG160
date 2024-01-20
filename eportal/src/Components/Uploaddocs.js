da058dd9e6f46412fc7af92af7681082534c1b4d
nihalruttala <nihalruttala22@gmail.com>
2024-01-20 15:54:46 +0530

    updated

--- eportal/src/Components/Uploaddocs.js
+++ eportal/src/Components/Uploaddocs.js
@@ -1,13 +1,21 @@
 import React, { useState, useEffect } from 'react';
 import "./Uploaddocs.css";
+import './PDFViewer.css'
 import Filetable from './Filetable';
+import { Document, Page } from 'react-pdf';
+import { GlobalWorkerOptions } from 'pdfjs-dist';
+
+GlobalWorkerOptions.workerSrc = `https://unpkg.com/pdfjs-dist@2.10.377/build/pdf.worker.min.js`;
+
+
 
 const Uploaddocs = () => {
   const [formVisible, setFormVisible] = useState(false);
   const [files, setFiles] = useState([]);
-
+  const [pdfFile, setPDFFile] = useState(null);
+  const [pageNumber, setPageNumber] = useState(1);
+ 
   useEffect(() => {
-
     const fetchFiles = async () => {
       try {
         const response = await fetch('http://localhost:3001/files');
@@ -26,12 +34,13 @@ const Uploaddocs = () => {
   };
 
   const submitForm = async () => {
-    
+    const cnrNumber = document.getElementById("cnr").value;
     const fileName = document.getElementById("fileName").value;
     const fileUpload = document.getElementById("fileUpload");
   
     if (fileName && fileUpload.files.length > 0) {
       const formData = new FormData();
+      formData.append('cnr', cnrNumber);
       formData.append('fileName', fileName);
   
       for (let i = 0; i < fileUpload.files.length; i++) {
@@ -57,34 +66,53 @@ const Uploaddocs = () => {
         alert('Upload failed');
       }
   
-  
-      document.getElementById("fileName").value = "";
+      document.getElementById("cnr").value = '';
+      document.getElementById("fileName").value = '';
       fileUpload.value = "";
     } else {
       alert("Please provide a file name and choose at least one file.");
     }
   };
   const viewFile = async(filename) => {
+     let fileContentArrayBuffer;
+
     try {
       const response = await fetch(`http://localhost:3001/files/${encodeURIComponent(filename)}`);
       if (response.ok) {
-        const fileContentBase64 = await response.text();
-        
-        // Decode Base64 content
-        const decodedContent = atob(fileContentBase64);
-        console.log('Decoded Content:', decodedContent);
-        const newTab = window.open();
-        newTab.document.write(`
-        <!DOCTYPE html>
-        <html>
-          <head>
-            <title>${filename}</title>
-          </head>
-          <body>
-            <pre>${decodedContent}</pre>
-          </body>
-        </html>
-      `);
+        const fileContent = await response.arrayBuffer();
+        const fileExtension = filename.split('.').pop().toLowerCase();
+
+        if (fileExtension === 'pdf') {
+          
+            
+            const newTab = window.open();
+            newTab.document.write(`
+              <!DOCTYPE html>
+              <html>
+                <head>
+                  <title>${filename}</title>
+                </head>
+                <body>
+                <embed width="1000" height="800" src="data:application/pdf;base64,${btoa(fileContent)}" type="application/pdf" />
+                </body>
+                </body>
+              </html>
+            `);
+          } else {
+            // For other file types, open a new tab with the content
+            const newTab = window.open();
+            newTab.document.write(`
+              <!DOCTYPE html>
+              <html>
+                <head>
+                  <title>${filename}</title>
+                </head>
+                <body>
+                  <pre>${fileContent}</pre>
+                </body>
+              </html>
+            `);
+          }
       } else {
         console.error('Error fetching file content:', response.status);
         alert('Error fetching file content');
@@ -94,6 +122,23 @@ const Uploaddocs = () => {
       alert('Error fetching file content');
     }
   };
+
+  const handleChange = (e) => {
+    let selectedFile = e.target.files[0];
+    const fileType = ['application/pdf'];
+
+    if (selectedFile && fileType.includes(selectedFile.type)) {
+      let reader = new FileReader();
+      reader.readAsDataURL(selectedFile);
+      reader.onload = (e) => {
+        setPDFFile(e.target.result);
+      };
+    } else {
+      setPDFFile(null);
+    }
+  };
+  
+  
 
   return (
     <section>
@@ -119,7 +164,7 @@ const Uploaddocs = () => {
                   <input type="text" id="fileName" name="fileName" required/>
                   <br/>
                   <label  className="data" htmlFor="fileUpload"><b>Choose File:&nbsp;&nbsp;</b></label>
-                  <input  type="file" id="fileUpload" name="fileUpload" accept=".txt, .pdf, .doc , image/, video/ "/>
+                  <input  type="file" id="fileUpload" name="fileUpload" accept=".txt, .pdf, .doc , image/, video/" onChange={handleChange} />
                   <button className='uploadbtn' type="button" onClick={submitForm}>Submit</button>
                   <br/>
                   <br/>
@@ -133,11 +178,24 @@ const Uploaddocs = () => {
 
       <div>
         
-        <Filetable files={files} onViewFile={viewFile} />
+      <Filetable files={files} onViewFile={viewFile} />
+        {pdfFile ? (
+          <div>
+          {console.log('pdfFile:', pdfFile)}
+          <Document
+          file={pdfFile }
+          onLoadSuccess={() => setPageNumber(1)}
+          onLoadError={(error) => console.error('PDF loading error:', error)}
+        >
+          <Page pageNumber={pageNumber} />
+        </Document>
+            </div>
+        ) : (
+          <p></p>
+        )}
       </div>
-      
-    </section>
+      </section>
   );
-}
+};
 
 export default Uploaddocs;
